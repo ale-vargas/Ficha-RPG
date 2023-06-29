4b1959f146b03d7572a7fa412878125db74b91ff
ale-vargas <alexcvjunior@gmail.com>
2023-06-29 09:19:01 -0300

    js update

--- script.js
+++ script.js
@@ -1,65 +1,44 @@
-// FORÇA
-var modFOR = document.querySelector('div#mod-for')
-modFOR.innerHTML = '0';
-var txtFOR = document.querySelector('input#text-for')
-var resFOR  = 0;
-txtFOR.addEventListener('input',function() {
-    var numFOR = parseInt(txtFOR.value)
-    var resFOR = Math.floor((numFOR - 10) / 2)
-    modFOR.innerHTML = `${resFOR}`  
-})
-
-// DESTREZA
-var modDES = document.querySelector('div#mod-des')
-modDES.innerHTML = '0';
-var txtDES = document.querySelector('input#text-des')
-var resDES = 0;
-txtDES.addEventListener('input',function() {
-    var numDES = parseInt(txtDES.value)
-    var resDES = Math.floor((numDES - 10) / 2)
-    modDES.innerHTML = `${resDES}`  
-})
-
-// CONSTITUIÇÃO
-var modCON = document.querySelector('div#mod-con')
-modCON.innerHTML = '0';
-var txtCON = document.querySelector('input#text-con')
-var resCON = 0;
-txtCON.addEventListener('input',function() {
-    var numCON = parseInt(txtCON.value)
-    var resCON = Math.floor((numCON - 10) / 2)
-    modCON.innerHTML = `${resCON}`  
-})
-
-// INTELIGÊNCIA
-var modINT = document.querySelector('div#mod-int')
-modINT.innerHTML = '0';
-var txtINT = document.querySelector('input#text-int')
-var resINT = 0;
-txtINT.addEventListener('input',function() {
-    var numINT = parseInt(txtINT.value)
-    var resINT = Math.floor((numINT - 10) / 2)
-    modINT.innerHTML = `${resINT}`  
-})
-
-// SABEDORIA
-var modSAB = document.querySelector('div#mod-sab')
-modSAB.innerHTML = '0';
-var txtSAB = document.querySelector('input#text-sab')
-var resSAB = 0;
-txtSAB.addEventListener('input',function() {
-    var numSAB = parseInt(txtSAB.value)
-    var resSAB = Math.floor((numSAB - 10) / 2)
-    modSAB.innerHTML = `${resSAB}`  
-})
-
-// CARISMA
-var modCAR = document.querySelector('div#mod-car')
-modCAR.innerHTML = '0';
-var txtCAR = document.querySelector('input#text-car')
-var resCAR = 0;
-txtCAR.addEventListener('input',function() {
-    var numCAR = parseInt(txtCAR.value)
-    var resCAR = Math.floor((numCAR - 10) / 2)
-    modCAR.innerHTML = `${resCAR}`  
-})
+// Input dos atributos
+function identificarInput(txtSelector, atributo) {
+    var txtElem = document.querySelector(txtSelector)
+    txtElem.addEventListener('input', function() {
+      var num = parseInt(txtElem.value)
+      calcularModificador(num, atributo)
+    })
+  }
+  
+  // Cálculo do modificador
+  function calcularModificador(valor, atributo) {
+    var resVal = Math.floor((valor - 10) / 2)
+    escreverModificador(resVal, atributo)
+    armazenarModificador(resVal, atributo)
+  }
+  
+  // Escreve o modificador em sua div
+  function escreverModificador(valor, atributo) {
+    var modElem = document.querySelector(`div#mod-${atributo}`)
+    modElem.innerHTML = `${valor}`
+  }
+  
+  // Armazena Valor do atributo (ex.: valorFOR, valorDES)
+  function armazenarModificador(valor, atributo) {
+        window[`valor${atributo.toUpperCase()}`] = valor
+  }
+  
+  // FORÇA
+  identificarInput('input#text-for', 'for')
+  
+  // DESTREZA
+  identificarInput('input#text-des', 'des')
+  
+  // CONSTITUIÇÃO
+  identificarInput('input#text-con', 'con')
+  
+  // INTELIGÊNCIA
+  identificarInput('input#text-int', 'int')
+  
+  // SABEDORIA
+  identificarInput('input#text-sab', 'sab')
+  
+  // CARISMA
+  identificarInput('input#text-car', 'car')
